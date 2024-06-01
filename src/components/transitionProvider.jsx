d75ac033dcd6356cc865d37a24774ea523b620cb
znd666 <rasksk37@gmail.com>
2024-06-01 16:23:04 +0900

    fixed animation bug & create about page

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -1,11 +1,18 @@
 "use client";
 import { AnimatePresence, motion } from "framer-motion";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "@/components/navbar";
 import { usePathname } from "next/navigation";
 
 const TransitionProvider = ({ children }) => {
   const pathName = usePathname();
+  // const [isVisible, setIsVisible] = useState(true);
+  // const handleAnimationComplete = () => {
+  //   setIsVisible(false);
+  // };
+  // const handleAnimationStart = () => {
+  //   setIsVisible(true);
+  // };
   return (
     <AnimatePresence mode="wait">
       <div
@@ -18,15 +25,23 @@ const TransitionProvider = ({ children }) => {
           exit={{ height: "140vh" }}
           transition={{ duration: 0.5, ease: "easeOut" }}
         />
-        <motion.div
+        
+          <motion.div
           className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default w-fit h-fit z-30"
           initial={{ opacity: 1 }}
-          animate={{ opacity: 0 }}
+          animate={{ opacity: 0,
+            transitionEnd: {
+              display: "none",
+            }
+           }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.8, ease: "easeOut"}}
-        >
+          // onAnimationStart={handleAnimationStart}
+          // onAnimationComplete={handleAnimationComplete}
+          >
           { pathName == '/' ? 'Home' : pathName.substring(1)}
         </motion.div>
+        
         <motion.div
           className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-20"
           initial={{ height: "140vh" }}
